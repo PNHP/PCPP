4aaf17d74d3184e3d935d603d9cc2bc9c875c6e6
Christopher Tracey <ctracey@paconserve.org>
2018-06-15 10:52:24 -0400

    forgot to save, and the EO file

--- dataprep_plantEOgetter.R
+++ dataprep_plantEOgetter.R
@@ -33,69 +33,65 @@ srcpt <- paste(loc_eo,"eo_sourcept",sep="/")
 srcpt <- arc.open(srcpt)
 srcpt_plants <- arc.select(srcpt, fields=c('ELCODE','SNAME','SCOMNAME','ELSUBID','EO_ID','SF_ID','EST_RA','C_FEAT_TYPE'), where_clause="ELCODE LIKE 'P%'"  )
 sp.srcpt_plants <- arc.data2sp(srcpt_plants)
+sp.srcpt_plants@data <- merge(sp.srcpt_plants@data, eo_ptreps_plants[c("ELCODE","LASTOBS")],by="ELCODE")
+
 ## source lines
 srcln <- paste(loc_eo,"eo_sourceln",sep="/")
 srcln <- arc.open(srcln)
 srcln_plants <- arc.select(srcln, fields=c('ELCODE','SNAME','SCOMNAME','ELSUBID','EO_ID','SF_ID','EST_RA','C_FEAT_TYPE'), where_clause="ELCODE LIKE 'P%'"  )
 sp.srcln_plants <- arc.data2sp(srcln_plants)
+sp.srcln_plants@data <- merge(sp.srcln_plants@data, eo_ptreps_plants[c("ELCODE","LASTOBS")],by="ELCODE")
 ## source polys
 srcpy <- paste(loc_eo,"eo_sourcepy",sep="/")
 srcpy <- arc.open(srcpy)
 srcpy_plants <- arc.select(srcpy, fields=c('ELCODE','SNAME','SCOMNAME','ELSUBID','EO_ID','SF_ID','EST_RA','C_FEAT_TYPE'), where_clause="ELCODE LIKE 'P%'"  )
 sp.srcpy_plants <- arc.data2sp(srcpy_plants)
+sp.srcpy_plants@data <- merge(sp.srcpy_plants@data, eo_ptreps_plants[c("ELCODE","LASTOBS")],by="ELCODE")
+
+
+
 
 # load in a county layer
 county <- arc.open("W:/LYRS/Boundaries_Political/County Hollow.lyr" )
 county <- arc.select(county)
 sp.county <- arc.data2sp(county)
-sp.county500m <- gBuffer(sp.county,byid=TRUE,width=5000)
+#sp.county500m <- gBuffer(sp.county,byid=TRUE,width=5000) # buffer the county by 500m
+#prj <- sp.srcpt_plants@proj4string
 
+###proj4string(sp.county500m) <- prj
+#sp.county500m.split <- lapply(sp.county500m@polygons, function(x) SpatialPolygons(list(x)))
+
+#for(i in 1:length(sp.county500m.split)){
+#   proj4string(sp.county500m.split[[i]]) <- prj
+#}
 
 # Spatial Join data to counties
-join_srcpt <- over(sp.srcpt_plants, sp.county500m[,"COUNTY_NAM"])
+join_srcpt <- over(sp.srcpt_plants, sp.county[,"COUNTY_NAM"])
+#test4 <- lapply(sp.county500m.split, function(x) over(sp.srcpt_plants, x))
+#df <- t(data.frame(matrix(unlist(test4), nrow=67, byrow=T),stringsAsFactors=FALSE))
 sp.srcpt_plants$county <- join_srcpt$COUNTY_NAM
 
-join_srcln <- over(sp.srcln_plants, sp.county500m[,"COUNTY_NAM"],returnList = TRUE)
+join_srcln <- over(sp.srcln_plants, sp.county[,"COUNTY_NAM"],returnList = TRUE)
 join_srcln_coll <- sapply(join_srcln, paste, collapse=";", simplify = TRUE)
 sp.srcln_plants$county <- join_srcln_coll
 sp.srcln_plants$county <- gsub("c\\(", "",sp.srcln_plants$county)
 sp.srcln_plants$county <- gsub("\\)", "",sp.srcln_plants$county)
 sp.srcln_plants$county <- gsub("\"", "",sp.srcln_plants$county)
 
-join_srcpy <- over(sp.srcpy_plants, sp.county500m[,"COUNTY_NAM"],returnList = TRUE)
+join_srcpy <- over(sp.srcpy_plants, sp.county[,"COUNTY_NAM"],returnList = TRUE)
 join_srcpy_coll <- sapply(join_srcpy, paste, collapse=";", simplify = TRUE)
 sp.srcpy_plants$county <- join_srcpy_coll
 sp.srcpy_plants$county <- gsub("c\\(", "",sp.srcpy_plants$county)
 sp.srcpy_plants$county <- gsub("\\)", "",sp.srcpy_plants$county)
 sp.srcpy_plants$county <- gsub("\"", "",sp.srcpy_plants$county)
 
+# write out the results
+arc.write(paste(output_directory,"/gisdata/sp.srcpy_plants.shp",sep=""),sp.srcpy_plants)  
+arc.write(paste(output_directory,"/gisdata/sp.srcpt_plants.shp",sep=""),sp.srcpt_plants)    
+arc.write(paste(output_directory,"/gisdata/sp.srcln_plants.shp",sep=""),sp.srcln_plants)  
 
-# write out the resutls
-arc.write("P:/Conservation Programs/Natural Heritage Program/Projects/Active projects/1495 DCNR Plant Conservation Plan/Project Materials and Data/data/sp.srcpy_plants.shp",sp.srcpy_plants)
-arc.write("P:/Conservation Programs/Natural Heritage Program/Projects/Active projects/1495 DCNR Plant Conservation Plan/Project Materials and Data/data/sp.srcpt_plants.shp",sp.srcpt_plants)
-arc.write("P:/Conservation Programs/Natural Heritage Program/Projects/Active projects/1495 DCNR Plant Conservation Plan/Project Materials and Data/data/sp.srcln_plants.shp",sp.srcln_plants)
 
 
-##### get last obs dates for each EO and make a graph
-eo_lastobs <- sp.eo_ptreps_plants@data # convert to a data frame
-db <- dbConnect(SQLite(), dbname = databasename)
-SQLquery_et <- paste("SELECT ELCODE, GRANK, SRANK, `TRACKING.STATUS`"," FROM et_plants ")
-data_et <- dbGetQuery(db, statement = SQLquery_et )
-dbDisconnect(db)
-eo_lastobs <- merge(eo_lastobs, data_et, by="ELCODE")  # merge in grank, srank, etc
-eo_lastobs <- eo_lastobs[which(eo_lastobs$TRACKING.STATUS=="Y"|eo_lastobs$TRACKING.STATUS=="W"),]
-eo_lastobs$lastobsyear <- substr(eo_lastobs$LASTOBS,1,4) # should updrage the methods to use lubridate
-eo_lastobs$lastobsyear <- as.numeric(eo_lastobs$lastobsyear)
-eo_lastobs <- eo_lastobs[which(!is.na(eo_lastobs$lastobsyear)),]
 
-rounded_grank <- read.csv("reference_data/rounded_grank.csv",stringsAsFactors=FALSE) 
-rounded_srank <- read.csv("reference_data/rounded_srank.csv",stringsAsFactors=FALSE) 
-eo_lastobs <- merge(x=eo_lastobs, y=rounded_grank, by.x="GRANK", by.y="G.RANK", all.x=TRUE)
-eo_lastobs <- merge(x=eo_lastobs, y=rounded_srank, by.x="SRANK", by.y="S.RANK", all.x=TRUE)
-rounded_grank <- NULL
-rounded_srank <- NULL
-gt_ranks <- read.csv("reference_data/g_t_ranks.csv", stringsAsFactors=FALSE)
-eo_lastobs <- merge(eo_lastobs,gt_ranks,by.x="Rounded.G.RANK", by.y="rank", all.x=TRUE)
-gt_ranks <- NULL
 
 
--- missing_species_syn.r
+++ missing_species_syn.r
@@ -1,13 +1,22 @@
 if (!requireNamespace("taxize", quietly=TRUE)) install.packages("taxize")
 require(taxize)
-if (!requireNamespace("dplyr", quietly=TRUE)) install.packages("dplyr")
-require(dplyr)
+if (!requireNamespace("plyr", quietly=TRUE)) install.packages("plyr")
+require(plyr)
 
 species <- read.csv("maps_not_downloaded.csv", stringsAsFactors = FALSE, header = FALSE)
 
 a <- synonyms(species$V1, db="itis")
 b <- a
-b <- data.frame(matrix(unlist(b), byrow=T))
 
-b1 <- data.frame(t(sapply(b,c)))
-b1 <- ldply(b)
+# turn list of dataframes into a dataframe
+sapply(b, class)
+x2 <- lapply(b, as.data.frame)
+x3 <- bind_rows(x2)
+
+x3$match <- x3$syn_name %in% species$V1
+x3 <- x3[which( x3$match=="TRUE" ),]
+x3 <- x3[which(!is.na(x3$acc_name) ),]
+x3 <- unique(x3[c("acc_name","syn_name")])
+
+
+write.csv(x3, "bonap_syn.csv")
